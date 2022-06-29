8519df09cc852fb2b7ae042aa33efd689507f294
Abdessalam Mounach <mounach-@hotmail.com>
2022-06-28 18:58:59 -0700

    chore: add linked list utils

---
+++ linked_list_utils.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_list
+{
+	int data;
+	struct s_list *next;
+	struct s_list **head;
+} t_list;
+
+t_list *new_node(int num)
+{
+	t_list *node;
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->data = num;
+	node->next = NULL;
+}
+
+void	push_back(t_list **head, t_list *node)
+{
+	if (!node)
+		return;
+	if (!*head)
+	{
+		*head = node;
+		return;
+	}
+	t_list *last;
+	last = *head;
+	while (last->next)
+		last = last->next;
+	last->next = node;
+}
+
+void	push_front(t_list **head, t_list *node)
+{
+	if (!node)
+		return;
+	node->next = *head;
+	*head = node;
+}
+
+void	display(t_list *head)
+{
+	while (head)
+	{
+		printf("%d\n", head->data);
+		head = head->next;
+	}
+}
+
+int main()
+{
+	t_list *head = NULL;
+	int i = 0;
+
+	while (i < 100)
+	{
+		push_front(&head, new_node(++i));
+	}
+	display(head);
+}
