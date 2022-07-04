4a469d85073232455a6d2084213521fa79c0b797
Abdessalam Mounach <mounach-@hotmail.com>
2022-07-04 21:45:41 +0100

    chore: add header file

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 18:20:02 by amounach          #+#    #+#             */
-/*   Updated: 2022/07/03 22:22:14 by amounach         ###   ########.fr       */
+/*   Updated: 2022/07/04 21:43:42 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 typedef struct s_node
 {
     int     num;
-    struct  s_stack *above;
-    struct  s_stack *bellow;
+    struct  s_node *above;
+    struct  s_node *bellow;
 }   t_node;
 
 typedef struct s_stack
@@ -29,5 +29,20 @@ typedef struct s_stack
     t_node  *bottom;
     int     size;
 }   t_stack;
+
+void	pa(t_stack *a, t_stack *b, int flag);
+void	pb(t_stack *a, t_stack *b, int flag);
+
+void	ra(t_stack *a, int flag);
+void	rb(t_stack *b, int flag);
+void	rr(t_stack *a, t_stack *b, int flag);
+
+void	rra(t_stack *a, int flag);
+void	rrb(t_stack *b, int flag);
+void	rrr(t_stack *a, t_stack *b, int flag);
+
+void	sa(t_stack *b, int flag);
+void	sb(t_stack *b, int flag);
+void	ss(t_stack *a, t_stack *b, int flag);
 
 #endif
